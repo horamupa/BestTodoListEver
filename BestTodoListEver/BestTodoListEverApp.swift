fea24b69dcc0e3127a758ad9e995b0685be7fb6f
horamupa <horamupa@gmail.com>
2022-08-26 14:18:28 +0300

    Initial Commit

---
+++ BestTodoListEver/BestTodoListEverApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestTodoListEverApp.swift
+//  BestTodoListEver
+//
+//  Created by MM on 26.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct BestTodoListEverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
